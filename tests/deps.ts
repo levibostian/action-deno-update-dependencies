d9a849b74ca3e04b8b1dbd194f3467ff0e43a2db
levibostian <levibostian@users.noreply.github.com>
2023-04-25 12:33:32 +0000

    [create-pull-request] automated change

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,7 +1,7 @@
 // This file contains purposely outdated dependencies that this project will attempt to update. 
 
 // denoland example 
-export { parse as flagsParse } from "https://deno.land/std@0.182.0/flags/mod.ts";
+export { parse as flagsParse } from "https://deno.land/std@0.184.0/flags/mod.ts";
 
 // npm example 
-export { Octokit } from "npm:@octokit/rest@18.0.0"
+export { Octokit } from "npm:@octokit/rest@19.0.7"
