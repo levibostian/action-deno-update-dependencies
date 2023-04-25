3a214503d3c7add9a78946a0f48d96a321b08962
Levi Bostian <levi@levi.earth>
2023-04-25 07:25:43 -0500

    create tests

---
+++ tests/deps.ts
@@ -0,0 +1,7 @@
+// This file contains purposely outdated dependencies that this project will attempt to update. 
+
+// denoland example 
+export { parse as flagsParse } from "https://deno.land/std@0.182.0/flags/mod.ts";
+
+// npm example 
+export { Octokit } from "npm:@octokit/rest@18.0.0"
