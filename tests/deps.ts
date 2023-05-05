c211751c297e56bdb2c003e3c107dc7441b526af
Levi Bostian <levi@levi.earth>
2023-05-05 06:36:53 -0500

    define test

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,7 +1,9 @@
 // This file contains purposely outdated dependencies that this project will attempt to update. 
 
 // denoland example 
-export { parse as flagsParse } from "https://deno.land/std@0.182.0/flags/mod.ts";
+export * from "https://deno.land/std@0.182.0/flags/mod.ts";
+export * from "https://deno.land/std@0.182.0/flags/mod.ts#~";
 
 // npm example 
-export { Octokit } from "npm:@octokit/rest@18.0.0"
+export * from "npm:@octokit/rest@18.0.0" // expect to update to next major 
+export * from "npm:@octokit/rest@~18.0.0" // expect to just update to latest minor/patch
