75c8ab206d4e55d6239ca3d276ae76d570b0a7a0
Levi Bostian <levi@levi.earth>
2023-05-06 06:31:49 -0500

    give better examples in tests

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,8 +1,8 @@
 // This file contains purposely outdated dependencies that this project will attempt to update. 
 
 // denoland example 
-export * from "https://deno.land/std@0.182.0/flags/mod.ts";
-export * from "https://deno.land/std@0.182.0/flags/mod.ts#~";
+export * from "https://deno.land/x/obsidian@v7.0.0/mod.ts"; // expect to update to next major
+export * from "https://deno.land/x/obsidian@v3.2.0/mod.ts#~"; // expect to just update to latest minor/patch
 
 // npm example 
 export * from "npm:@octokit/rest@18.0.0" // expect to update to next major 
