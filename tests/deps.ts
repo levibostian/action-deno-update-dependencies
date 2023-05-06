e2b8d68e69849ac52026876726e87f15da10c894
levibostian <levibostian@users.noreply.github.com>
2023-05-06 12:20:53 +0000

    [create-pull-request] automated change

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,9 +1,9 @@
 // This file contains purposely outdated dependencies that this project will attempt to update. 
 
 // denoland example 
-export * from "https://deno.land/x/obsidian@v7.0.0/mod.ts"; // expect to update to next major
-export * from "https://deno.land/x/obsidian@v3.2.0/mod.ts#~"; // expect to just update to latest minor/patch
+export * from "https://deno.land/x/obsidian@v8.0.0/mod.ts"; // expect to update to next major
+export * from "https://deno.land/x/obsidian@v3.2.1/mod.ts#~"; // expect to just update to latest minor/patch
 
 // npm example 
-export * from "npm:@octokit/rest@18.0.0" // expect to update to next major 
-export * from "npm:@octokit/rest@~18.0.0" // expect to just update to latest minor/patch
+export * from "npm:@octokit/rest@19.0.7" // expect to update to next major 
+export * from "npm:@octokit/rest@~18.0.15" // expect to just update to latest minor/patch
